21088b3eac6f235c254239a2ae52ccef2fb350f6
Andrew Batmnanov <andrew.batmanov@gmail.com>
2025-10-04 14:05:05 +0300

    data added

---
+++ Artists_albums_tracks_collections.sql
@@ -0,0 +1,13 @@
+INSERT INTO Genre (name) VALUES
+('Rock'),
+('Metal'),
+('Hip-Hop'),
+('Pop');
+
+INSERT INTO Artist (name) VALUES
+('Bon Jovi'),
+('Creed'), 
+('Trent Reznor'),
+('Metallica'),
+('Texas Toast Chainsaw Massacre'),
+('Wage War')
